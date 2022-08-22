fb8e902ae25ea5927549361ad3d8c876a64460bf
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-22 23:26:00 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -27,7 +27,8 @@ AFRAME.registerComponent('markers_start',{
 			markerEl.setAttribute('type','pattern');
 			markerEl.setAttribute('preset','hiro');
 			markerEl.setAttribute('url',markersURLArray[k]);
-			markerEl.setAttribute('id','marker ' + markersNameArray[k]);
+			markerEl.setAttribute('id',markersNameArray[k]);
+			markerEl.setAttribute('class','marker');
 			markerEl.setAttribute('raycaster','objects: .clickable');
 			markerEl.setAttribute('emitevents','true');
 			markerEl.setAttribute('cursor','fuse: false; rayOrigin: mouse;');
@@ -44,9 +45,9 @@ AFRAME.registerComponent('markers_start',{
 			var img = document.createElement('a-image');
 			
 			img.setAttribute('src','#' + markersNameArray[k]);
-			img.setAttribute('id','image ' + markersNameArray[k]);
+			img.setAttribute('id',markersNameArray[k]);
 			img.setAttribute('scale','1 1 1');
-			img.setAttribute('class','clickable');
+			img.setAttribute('class','image clickable');
 			img.setAttribute('gesture-handler','');
 			img.object3D.position.set(0, 0.3, 0);
 			img.object3D.rotation.set(-90, 0, 0);
