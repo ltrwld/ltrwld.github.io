8d0971385684562c05869278728f9bea744d725c
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-22 06:29:28 +0200

    retry

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -38,23 +38,6 @@ AFRAME.registerComponent('markers_start',{
 			textEl.object3D.rotation.set(-90, 0, 0);
 
 			markerEl.appendChild(textEl);
-
-
-			var scene = document.querySelector('a-scene');
-			var cylinder = document.createElement('a-cylinder');
-			cylinder.setAttribute('color', '#FF9500');
-			cylinder.setAttribute('height', '2');
-			cylinder.setAttribute('radius', '0.75');
-			cylinder.setAttribute('position', '3 1 0');
-			scene.appendChild(cylinder);
-			var t = 0;
-			function render() {
-			  t += 0.01;
-			  requestAnimationFrame(render);
-			  cylinder.setAttribute('position', '3 '+(Math.sin(t*2)+1)+' 0');
-			}
-			render();
-
 		}
 	}
 });
