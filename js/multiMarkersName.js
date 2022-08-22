a4342c8697da9e4528ebf0bff0ba8b988cc58503
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-08-22 15:23:54 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent('markers_start',{
 		var sceneEl = document.querySelector('a-scene');
 		
 		//list of the markers
-		for(var i=1; i<24; i++)
+		for(var i=1; i<23; i++)
 		{
 			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
 			markersURLArray.push(url);
@@ -19,7 +19,7 @@ AFRAME.registerComponent('markers_start',{
 			//console.log(url);
 		}
 
-		for(var k=0; k<23; k++)
+		for(var k=0; k<22; k++)
 		{
 			var markerEl = document.createElement('a-marker');
 			const number = k;
