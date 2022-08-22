d2fc81ea38a8d4e2dc1ca186004582f4d07c6a99
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-22 05:22:42 +0200

    img

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -37,6 +37,44 @@ AFRAME.registerComponent('markers_start',{
 			textEl.object3D.position.set(0, 0.7, 0);
 			textEl.object3D.rotation.set(-90, 0, 0);
 
+
+
+
+			const image = document.createElement('img');
+
+// 👇️ Local image
+// image.setAttribute('src', 'my-img.png');
+
+// 👇️ Remote image
+image.setAttribute(
+  'src',
+  'http://bobbyhadz.com/images/blog/javascript-show-div-on-select-option/banner.webp',
+);
+
+image.setAttribute('alt', 'nature');
+
+image.setAttribute('height', 350); // 👈️ height in px
+image.setAttribute('width', 550); // 👈️ width in px
+
+// 👇️ optionally style the image
+image.style.border = '5px solid yellow';
+
+image.onerror = function handleError() {
+  console.log('Image could not be loaded');
+  // 👇️ Can set image.src to a backup image here
+  // image.src = 'backup-image.png'
+
+  // 👇️ Or hide image
+  // image.style.display = 'none';
+};
+
+image.onload = function handleImageLoaded() {
+  console.log('image loaded successfully');
+};
+
+const box = document.getElementById('box');
+box.appendChild(image);
+
 			markerEl.appendChild(textEl);
 		}
 	}
