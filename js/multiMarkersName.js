89fd45ae537fcbac877ff4f4b7d807e84d4cbf68
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-23 01:24:18 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -44,7 +44,7 @@ AFRAME.registerComponent('markers_start',{
 			img.setAttribute('scale','1 1 1');
 			img.setAttribute('class','image clickable');
 			img.object3D.position.set(0, 0.3, 0);
-			img.object3D.rotation.set(-80, 0, 0);
+			img.object3D.rotation.set(85, 0, 0);
 
 			markerEl.appendChild(img);
 		}
