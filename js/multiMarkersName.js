8686c4ff80c4b81af7ff77924e7bb915ffd7abd4
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-08-22 10:46:37 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -34,7 +34,7 @@ AFRAME.registerComponent('markers_start',{
 			
 			img.setAttribute('src','#' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
-			img.setAttribute('onclick',"location.href='https://www.google.de/" + markersNameArray[k] "'");
+			img.setAttribute('onclick',"location.href='https://www.google.de/" + markersNameArray[k] + "'");
 			img.object3D.position.set(0, 0.7, 0);
 			img.object3D.rotation.set(-90, 0, 0);
 
