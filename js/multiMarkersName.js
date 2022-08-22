74dbb7741cf897acfda866b555c5bc1da5672694
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-22 06:47:53 +0200

    init

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -30,10 +30,9 @@ AFRAME.registerComponent('markers_start',{
 			sceneEl.appendChild(markerEl);
 
 			//Adding text to each marker
-			var textEl = document.createElement('a-entity');
+			var textEl = document.createElement('a-image');
 			
-			textEl.setAttribute('id','text');
-			textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
+			textEl.setAttribute('src','#img-'+k);
 			textEl.object3D.position.set(0, 0.7, 0);
 			textEl.object3D.rotation.set(-90, 0, 0);
 
