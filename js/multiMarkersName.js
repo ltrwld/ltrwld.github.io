23958bb1d5f982f54a4e044aa49fc2eeeb75fabe
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-23 16:32:07 +0200

    full

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -8,6 +8,8 @@ AFRAME.registerComponent('markers_start',{
 	init:function(){
 
 		//force to fullscreen
+		var elem = document.getElementById("body");
+
 		if (elem.requestFullscreen) {
 			elem.requestFullscreen();
 		  } else if (elem.webkitRequestFullscreen) { /* Safari */
@@ -15,7 +17,7 @@ AFRAME.registerComponent('markers_start',{
 		  } else if (elem.msRequestFullscreen) { /* IE11 */
 			elem.msRequestFullscreen();
 		  }
-		  
+
 		console.log('Add markers to the scene');
 
 		var sceneEl = document.querySelector('a-scene');
