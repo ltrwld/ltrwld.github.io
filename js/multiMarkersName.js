064c9bee24c470c744b8dcfedb11b3b9da4a5184
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-08-23 02:41:12 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -33,14 +33,15 @@ AFRAME.registerComponent('markers_start',{
 
 			//Adding text to each marker
 			var img = document.createElement('a-image');
+			var marker = markersNameArray[k];
 			
-			img.setAttribute('src','#' + markersNameArray[k]);
-			img.setAttribute('id',markersNameArray[k]);
+			img.setAttribute('src','#' + marker);
+			img.setAttribute('id',marker);
 			img.setAttribute('scale','1 1 1');
 			img.setAttribute('class','clickable');
-			img.setAttribute('link',"href: https://google.com/search?q=" + markersNameArray[k]);
+			img.setAttribute('link',"href: https://google.com/search?q=" + marker);
 			img.object3D.position.set(0, 0, 0);
-			img.object3D.rotation.set(180, 0, 0);
+			img.object3D.rotation.set(160, 0, 0);
 
 			markerEl.appendChild(img);
 		}
