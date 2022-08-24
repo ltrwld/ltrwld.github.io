72a9ab8fd69c26dc8e6d98983302e584771f99ef
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-08-24 16:04:13 +0200

    update

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -38,10 +38,10 @@ AFRAME.registerComponent('markers_start',{
 			img.setAttribute('src','#' + markersNameArray[k]);
 			img.setAttribute('link','href: https://google.com/search?q=' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
-			img.setAttribute('scale','3 3 3'); 
+			img.setAttribute('scale','1 1 1'); 
 			img.setAttribute('class','image clickable');
 			img.object3D.position.set(0, 0.3, 0);
-			img.object3D.rotation.set(-90, 0, 0);
+			img.object3D.rotation.set(65, 0, 0);
 
 			markerEl.appendChild(img);
 		}
