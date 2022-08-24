79106232f4c9567ad5d941808485768c6f180757
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-08-24 16:09:59 +0200

    2marker

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent('markers_start',{
 		var sceneEl = document.querySelector('a-scene');
 		
 		//list of the markers
-		for(var i=1; i<23; i++)
+		for(var i=1; i<2; i++)
 		{
 			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
 			markersURLArray.push(url);
@@ -19,7 +19,7 @@ AFRAME.registerComponent('markers_start',{
 			//console.log(url);
 		}
 
-		for(var k=0; k<22; k++)
+		for(var k=0; k<1; k++)
 		{
 			var markerEl = document.createElement('a-marker');
 			const number = k;
