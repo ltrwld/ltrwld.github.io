a0073c4d7d5fec1a89b494467c268d474f415e83
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.local>
2022-08-29 10:22:53 +0200

    backup

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent('markers_start',{
 		var sceneEl = document.querySelector('a-scene');
 		
 		//list of the markers
-		for(var i=1; i<52; i++)
+		for(var i=1; i<23; i++)
 		{
 			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
 			markersURLArray.push(url);
@@ -19,7 +19,7 @@ AFRAME.registerComponent('markers_start',{
 			//console.log(url);
 		}
 
-		for(var k=0; k<51; k++)
+		for(var k=0; k<22; k++)
 		{
 			var markerEl = document.createElement('a-marker');
 			const number = k;
@@ -38,10 +38,10 @@ AFRAME.registerComponent('markers_start',{
 			img.setAttribute('src','#' + markersNameArray[k]);
 			img.setAttribute('link','href: https://ltrwld.github.io/redirect.html?link=' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
-			img.setAttribute('scale','3 3 3'); 
+			img.setAttribute('scale','1 1 1'); 
 			img.setAttribute('class','image clickable');
 			img.object3D.position.set(0, 0.3, 0);
-			img.object3D.rotation.set(-90, 0, 0);
+			img.object3D.rotation.set(65, 0, 0);
 
 			markerEl.appendChild(img);
 		}
