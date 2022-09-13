822b2307a1323a64f11077c7229e7d690e455b3a
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-09-13 12:01:55 +0200

    old

--- artoolkit/js/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function ___export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-___export(require('./artoolkit.api'));
-___export(require('./artoolkit.three'));
--- js/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './artoolkit.api';
-export * from './artoolkit.three';
-
-import {ARControllerStatic} from './artoolkit.three';
-
-export declare var ARController : ARControllerStatic;
