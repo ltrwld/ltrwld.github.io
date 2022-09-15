44e056fbf1f4d35a491c759ffc045e11e8476ae9
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-09-15 14:37:18 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -33,44 +33,45 @@ AFRAME.registerComponent('markers_start',{
 				var vali1=1;
 				var vali2=23;
 
+				var valk1=0;
+				var valk2=22;
+
 				//list of the markers
 				for(var i=vali1; i<vali2; i++) {
 				var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
 				markersURLArray.push(url);
 				markersNameArray.push('Marker_'+i);
 				console.log(url);
+
+				for(var k=valk1; k<valk1; k++) {
+					var markerEl = document.createElement('a-marker');
+					const number = k;
+		
+					markerEl.setAttribute('type','pattern');
+					markerEl.setAttribute('url',markersURLArray[k]);
+					markerEl.setAttribute('id','markersArray');
+					markerEl.setAttribute('class','marker');
+			
+					markerEl.setAttribute('registerevents','');
+					sceneEl.appendChild(markerEl);
+	
+					//Adding img to each marker
+					var img = document.createElement('a-image');
+			
+					img.setAttribute('src','#' + markersNameArray[k]);
+					img.setAttribute('link','href: https://ltrwld.github.io/redirect.html?link=' + markersNameArray[k]);
+					img.setAttribute('id','markersArray');
+					img.setAttribute('scale','1 1 1'); 
+					img.setAttribute('class','image clickable');
+					img.object3D.position.set(0, 0.3, 0);
+					img.object3D.rotation.set(65, 0, 0);
+	
+					markerEl.appendChild(img);
+				}
 			}
 			}
 
 			console.log('#### LOAD GROUNDL');
-
-			
-
-			for(var k=0; k<90; k++) {
-				var markerEl = document.createElement('a-marker');
-				const number = k;
-	
-				markerEl.setAttribute('type','pattern');
-				markerEl.setAttribute('url',markersURLArray[k]);
-				markerEl.setAttribute('id','markersArray');
-				markerEl.setAttribute('class','marker');
-		
-				markerEl.setAttribute('registerevents','');
-				sceneEl.appendChild(markerEl);
-
-				//Adding img to each marker
-				var img = document.createElement('a-image');
-		
-				img.setAttribute('src','#' + markersNameArray[k]);
-				img.setAttribute('link','href: https://ltrwld.github.io/redirect.html?link=' + markersNameArray[k]);
-				img.setAttribute('id','markersArray');
-				img.setAttribute('scale','1 1 1'); 
-				img.setAttribute('class','image clickable');
-				img.object3D.position.set(0, 0.3, 0);
-				img.object3D.rotation.set(65, 0, 0);
-
-				markerEl.appendChild(img);
-			}
 		}
 
 		window.addEventListener('deviceorientation', handleOrientation);
