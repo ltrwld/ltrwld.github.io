66a34d3b37b1c39480a94f388610f86b16bd97ca
Tim Lauterwald <lauterwald@Tims-MacBook-Pro.fritz.box>
2022-09-15 14:30:35 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -20,13 +20,13 @@ AFRAME.registerComponent('markers_start',{
 
 			// Because we don't want to have the device upside down
 			// We constrain the x value to the range [-90,90]
-			if (x >  90) { x =  90};
-			if (x < -90) { x = -90};
+			// if (x >  90) { x =  90};
+			// if (x < -90) { x = -90};
 
 			// To make computation easier we shift the range of
 			// x and y to [0,180]
-			x += 90;
-			y += 90;
+			// x += 90;
+			// y += 90;
 
 			// if device is 45 degree load ground (1-20)
 			if (x > 45) {
