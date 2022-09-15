71f25839ce9fed7ed078a11c0f6ad7226b104546
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-09-15 16:58:49 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -54,6 +54,48 @@ AFRAME.registerComponent('markers_start',{
 				markersNameArray.push('Marker_'+i);
 				console.log(url); }
 
+				if (x < 49) {
+
+				for(var k=valk1; k<valk2; k++) {
+					var markerEl = document.createElement('a-marker');
+		
+					markerEl.setAttribute('type','pattern');
+					markerEl.setAttribute('url',markersURLArray[k]);
+					markerEl.setAttribute('id','markersArray');
+					markerEl.setAttribute('class','marker');
+			
+					markerEl.setAttribute('registerevents','');
+					sceneEl.appendChild(markerEl);
+	
+					//Adding img to each marker
+					var img = document.createElement('a-image');
+			
+					img.setAttribute('src','#' + markersNameArray[k]);
+					img.setAttribute('link','href: https://ltrwld.github.io/redirect.html?link=' + markersNameArray[k]);
+					img.setAttribute('id','markersArray');
+					img.setAttribute('scale','1 1 1'); 
+					img.setAttribute('class','image clickable');
+					img.object3D.position.set(0, 0.3, 0);
+					img.object3D.rotation.set(65, 0, 0);
+	
+					markerEl.appendChild(img);
+
+
+
+					// if device is 45 degree load ground (1-20)
+					while (x > 49) {
+
+					const deleteall = document.getElementById('markersArray');
+					deleteall.remove();
+	
+					}
+
+				}
+			}
+
+
+			if (x > 49) {
+
 				for(var k=valk1; k<valk2; k++) {
 					var markerEl = document.createElement('a-marker');
 		
@@ -89,6 +131,14 @@ AFRAME.registerComponent('markers_start',{
 					}
 
 				}
+			}
+
+
+
+
+
+
+
 
 				
 			
