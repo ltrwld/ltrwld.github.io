07a4e06674d8da9c0aa34e1bb07491675117fcbd
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-09-17 23:26:51 +0200

    scale

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -38,7 +38,7 @@ AFRAME.registerComponent('markers_start',{
 			img.setAttribute('src','#' + markersNameArray[k]);
 			img.setAttribute('link','href: https://ar.cerq.com/redirect.html?link=' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
-			img.setAttribute('scale','4 1 1'); 
+			img.setAttribute('scale','4 2'); 
 			img.setAttribute('class','image clickable');
 			img.object3D.position.set(0, 0.3, 0);
 			img.object3D.rotation.set(250, 0, 0);
