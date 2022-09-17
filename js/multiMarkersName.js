a2cf08b0542e3d5c68f3552eebe094426e3b79c2
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-09-17 13:58:38 +0200

    direct integration

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -61,14 +61,13 @@ AFRAME.registerComponent('markers_start',{
 			//Adding text to each marker
 			var img = document.createElement('a-video');
 			
-			img.setAttribute('src','#' + markersNameArray[k]);
+			img.setAttribute('src','content/' + markersNameArray[k]);
 			img.setAttribute('link','href: https://ar.cerq.com/redirect.html?link=' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
 			img.setAttribute('scale','4 4 1'); 
 			img.setAttribute('class','image clickable');
 			img.object3D.position.set(0, 0.3, 0);
 			img.object3D.rotation.set(250, 0, 0);
-
 			markerEl.appendChild(video);
 		}
 	}
