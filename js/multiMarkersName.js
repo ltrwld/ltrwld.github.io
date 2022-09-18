bf2128bf2cab5460ad90f995ea7f8fa9f6e2dc2f
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-09-18 15:28:21 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -36,8 +36,7 @@ AFRAME.registerComponent('markers_start',{
 			img.setAttribute('src','#' + markersNameArray[k]);
 			img.setAttribute('link','href: https://ar.cerq.com/redirect.html?link=' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
-			img.setAttribute('width','20'); 
-			img.setAttribute('height','20'); 
+			img.setAttribute('scale','5 5 1'); 
 			img.object3D.position.set(0, 0.3, 0);
 			img.object3D.rotation.set(250, 0, 0);
 			img.setAttribute('class','image clickable');
