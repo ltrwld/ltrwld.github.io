83c062ad3eec3ec6c84618ff19bfd3e94a59f6f8
Tim Lauterwald <lauterwald@Tims-MBP.fritz.box>
2022-09-18 18:02:48 +0200

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -33,13 +33,11 @@ AFRAME.registerComponent('markers_start',{
 			//Adding text to each marker
 			var img = document.createElement('a-image');
 			
-			img.object3D.scale.set(5, 5, 1);
-
 			img.setAttribute('src','#' + markersNameArray[k]);
 			img.setAttribute('link','href: https://ar.cerq.com/redirect.html?link=' + markersNameArray[k]);
 			img.setAttribute('id',markersNameArray[k]);
-			
-			img.object3D.position.set(0, 0, 0);
+			img.setAttribute('scale','2 1.5 1'); 
+			img.object3D.position.set(0, 1, 0);
 			img.object3D.rotation.set(250, 0, 0);
 			img.setAttribute('class','image clickable');
 			img.setAttribute('smooth','true');
@@ -70,8 +68,7 @@ AFRAME.registerComponent('markers_start',{
 			video.setAttribute('id',markersNameArray[k]);
 			video.setAttribute('width','16');
 			video.setAttribute('height','9');
-			video.object3D.scale.set(5, 5, 1);
-			video.object3D.position.set(0, 0, 0);
+			video.object3D.position.set(0, 0, -20);
 			video.setAttribute('smooth','true');
 			video.setAttribute('smoothCount','10');
 			video.setAttribute('smoothTolerance','.01');
